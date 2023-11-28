2b27b8644c18c1b4be858c413cfd85dedef01b0c
Bryan Passiberg de Matos <145020087+Bri4np@users.noreply.github.com>
2023-11-28 11:44:37 -0300

    Add files via upload

---
+++ 28.11.2023 11;43/enem.php
@@ -0,0 +1,61 @@
+ 
+
+<!DOCTYPE html> 
+<html long="pt-br" >
+    
+        <meta charset="UFT-8"/>
+        <meta nome="viewport" content="width=device-width,initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=ege">
+        <link rel="stylesheet" href="enemf.css"/>
+        <title>Enem-fudendo</title>
+        <link rel="shortcut icon" type="imagex/png" href="enem1.png">
+    </head>
+    <body>
+    <?php require('topo.php');?>
+ 
+        
+
+          
+    <div class="container-fluid bg-white text-white py-2 mb-4">
+         
+               <UL class="menu">  
+               
+                  
+                 
+                  
+                   <li><a href="indexjogos.html "><font color="black"> MATERIAS</font> </a></li>
+                   <li><a href="#section2"><font color="black">EXERCICIOS</font> </a></li>
+                   <li><a href="#section2"> <font color="black">LINKS</font> </a></li>
+                   <li><a href="#section2"><font color="black">REDES SOCIAIS</font> </a></li>
+                   
+                   
+               </UL>
+           
+</div>
+           <div id="xbox5"> 
+      
+
+<div class="container">
+    <h1>Listar Ferramentas</h1>
+   
+
+   
+ 
+            <div class="ferramenta">;
+           
+            <li><font color="black"> nome</font></li>
+            <li><font color="black">descriçao</font></li>
+            <li><font color="black"> categoria</font></li>
+           </div> 
+    
+          </div>
+
+          <div id="xbox3"> 
+             
+            <nav>
+                <ul class="menu">
+                    <li>  </li>  
+                     
+                </ul>
+             </nav>
+          </div>
